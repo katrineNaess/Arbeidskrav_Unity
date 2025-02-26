c4c4a86d548790fea755ba5ca457dc49413b952e
Katrine <kana020@student.kristiania.no>
2025-02-26 12:48:30 +0100

    test

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -5,7 +5,7 @@ public class Movement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // TEST
     }
 
     // Update is called once per frame
