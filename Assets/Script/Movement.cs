0ef452ec677630797292379554bf741826744c13
notut <noratt66@gmail.com>
2025-02-26 13:14:27 +0100

    script endring

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -1,7 +1,11 @@
-using UnityEngine;
+4using UnityEngine;
 
-public class Movement : MonoBehaviour
+//Skyte ballong
+public class ShootBalloon : MonoBehaviour
 {
+    public Camera playerCamera;
+
+    public float range = 100f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,21 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)) //Venstre museklikk
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, range))
+        {
+            if (hit.collider.CompareTag("Ballong")) 
+            {
+                Destroy(hit.collider.gameObject); //Sletter ballongen
+            }
+        }
     }
 }
